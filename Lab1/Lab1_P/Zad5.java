859e8eca730b10ad32a44cc7fb5f5bc4a05c4183
obykowski <olafbykowski1@gmail.com>
2023-10-11 14:14:53 +0200

    Calculator without pemdas

--- Lab1/Lab1_P/Zad5.java
+++ Lab1/Lab1_P/Zad5.java
@@ -1,35 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Zad5 {
-
-    private double result;
-
     public static void main(String[] args) {
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj dzialanie matematyczne bez nawiasow: ");
+        String scannedString = scanner.nextLine().replaceAll("\\s+", " ");
 
-    public void dodaj(double number) {
-        this.result += number;
-    }
+        String[] scannedArray = scannedString.toLowerCase().split(" ");
+        System.out.println(Arrays.toString(scannedArray));
 
-    public void odejmij(double number) {
-        this.result -= number;
-    }
-
-    public void pomnoz(double number) {
-        this.result *= number;
-    }
-
-    public void podziel(double number) throws Exception {
-        if (number == 0) {
-            throw new Exception("You can't divide by 0");
+        for (int i = 0; i < scannedArray.length - 1; i++) {
+            String s = scannedArray[i];
+            try {
+                Integer.parseInt(s);
+            } catch (Exception e) {
+                if (i == 0) throw new IllegalArgumentException("First argument must be a number");
+                switch (s) {
+                    case "+" -> {
+                        scannedArray[i + 1] = String.valueOf(Integer.parseInt(scannedArray[i - 1]) + Integer.parseInt(scannedArray[i + 1]));
+                    }
+                    case "-" -> {
+                        scannedArray[i + 1] = String.valueOf(Integer.parseInt(scannedArray[i - 1]) - Integer.parseInt(scannedArray[i + 1]));
+                    }
+                    case "*" -> {
+                        scannedArray[i + 1] = String.valueOf(Integer.parseInt(scannedArray[i - 1]) * Integer.parseInt(scannedArray[i + 1]));
+                    }
+                    case "/" -> {
+                        try {
+                            scannedArray[i + 1] = String.valueOf(Integer.parseInt(scannedArray[i - 1]) / Integer.parseInt(scannedArray[i + 1]));
+                        } catch (Exception exception) {
+                            throw new IllegalArgumentException("Cannot divide by 0");
+                        }
+                    }
+                    default -> throw new IllegalArgumentException("Unknown operation");
+                }
+            }
         }
-        this.result /= number;
+        System.out.println(scannedArray[scannedArray.length - 1]);
     }
-
-    public void wyczysc() {
-        this.result = 0;
-    }
-
-    public void wyswietl() {
-        System.out.println(this.result);
-    }
-
 }
